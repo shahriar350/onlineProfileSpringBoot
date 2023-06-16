b6c3f8a4d70ba88b5e40fed294bc678d4b13c13f
shahriar350 <shahriar350@gmail.com>
2023-06-16 19:02:09 +0600

    init project

--- backend/backend/src/main/java/com/bongobitan/backend/config/JwtAuthenticationFilter.java
+++ backend/backend/src/main/java/com/bongobitan/backend/config/JwtAuthenticationFilter.java
@@ -18,10 +18,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userPhone;
 
         if (authHeader == null || !authHeader.startsWith("Bearer")){
             filterChain.doFilter(request,response);
             return;
         }
+
+        jwt = authHeader.substring(7);
+
+
     }
 }
